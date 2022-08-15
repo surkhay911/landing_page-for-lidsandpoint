8a8370069fb3e031d7ec524e5e571d881bd92839
surkhay911 <surkhay77@yandex.ru>
2022-08-15 12:20:17 +0400

    v1.4

---
+++ js/script_button_up.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 1) {//количество пикселей после которой появится кнопка
+        document.getElementById("myBtn").style.display = "block";
+        console.log('1');
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
